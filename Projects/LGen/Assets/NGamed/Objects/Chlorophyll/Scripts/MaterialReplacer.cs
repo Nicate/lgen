8e1d0a9b5ef239c1336fe04dd572fb1a64c2d74c
Kirk <kirk@kirkwarez.com>
2019-05-17 16:49:59 +0200

    Created an isometric leaf stenciller that allows one to calculate how much area is exposed to the sun. The idea being that this will be (part of) the fitness function of the evolutionary algorithm. It employs a single shot camera with a render texture and some internal texture reading and evaluating. Right now it is activated using a debug key setup, but the idea is that this will be automated, of course. The API and scene setup is also rather tentative, I'm not sure if this will turn out to be the most useful or understandable version of it all.

---
+++ Projects/LGen/Assets/NGamed/Objects/Chlorophyll/Scripts/MaterialReplacer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialReplacer : MonoBehaviour {
+	public ReplacementMaterial[] replacementMaterials;
+
+	[Serializable]
+	public struct ReplacementMaterial {
+		public Material original;
+		public Material replacement;
+	}
+
+
+	private Dictionary<Material, Material> originalMaterialsMap;
+	private Dictionary<Material, Material> replacementMaterialsMap;
+
+
+	private void Awake() {
+		originalMaterialsMap = new Dictionary<Material, Material>(replacementMaterials.Length);
+		replacementMaterialsMap = new Dictionary<Material, Material>(replacementMaterials.Length);
+
+		foreach(ReplacementMaterial replacementMaterial in replacementMaterials) {
+			originalMaterialsMap.Add(replacementMaterial.replacement, replacementMaterial.original);
+			replacementMaterialsMap.Add(replacementMaterial.original, replacementMaterial.replacement);
+		}
+	}
+
+
+	public void replace() {
+		foreach(MeshRenderer renderer in GetComponentsInChildren<MeshRenderer>()) {
+			if(replacementMaterialsMap.ContainsKey(renderer.sharedMaterial)) {
+				renderer.sharedMaterial = replacementMaterialsMap[renderer.sharedMaterial];
+			}
+		}
+	}
+
+	public void restore() {
+		foreach(MeshRenderer renderer in GetComponentsInChildren<MeshRenderer>()) {
+			if(originalMaterialsMap.ContainsKey(renderer.sharedMaterial)) {
+				renderer.sharedMaterial = originalMaterialsMap[renderer.sharedMaterial];
+			}
+		}
+	}
+}
---
+++ Projects/LGen/Assets/NGamed/Objects/Chlorophyll/Scripts/Satellite.cs
@@ -0,0 +1,88 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class Satellite : MonoBehaviour {
+	public MaterialReplacer target;
+
+	public string exportFileName = "";
+
+
+	private Camera satelliteCamera;
+
+	private Texture2D texture;
+	private Rect rectangle;
+
+
+	private void Awake() {
+		satelliteCamera = GetComponent<Camera>();
+
+		// Only render when we tell it to.
+		satelliteCamera.enabled = false;
+	}
+
+	private void Start() {
+		int width = satelliteCamera.targetTexture.width;
+		int height = satelliteCamera.targetTexture.height;
+
+		texture = new Texture2D(width, height, TextureFormat.RGB24, false);
+		rectangle = new Rect(0, 0, width, height);
+	}
+
+
+	public void scan() {
+		target.replace();
+		satelliteCamera.Render();
+		target.restore();
+
+		RenderTexture.active = satelliteCamera.targetTexture;
+		texture.ReadPixels(rectangle, 0, 0);
+		RenderTexture.active = null;
+	}
+
+	public void evaluate() {
+		int responsiveArea = 0;
+		int unresponsiveArea = 0;
+
+		for(int y = (int) rectangle.yMin; y < (int) rectangle.yMax; y += 1) {
+			for(int x = (int) rectangle.xMin; x < (int) rectangle.xMax; x += 1) {
+				Color color = texture.GetPixel(x, y);
+
+				// Specifically looks for green pixels, this is not the most straightforward but allows us to visually inspect the render texture better.
+				if(color.g > 0.5f && color.b < 0.5f) {
+					responsiveArea += 1;
+				}
+				else {
+					unresponsiveArea += 1;
+				}
+			}
+		}
+
+		int totalArea = responsiveArea + unresponsiveArea;
+
+		float responsive = responsiveArea / (float) totalArea;
+		float unresponsive = unresponsiveArea / (float) totalArea;
+
+		Debug.LogFormat("Reponsive: {0}\nUnresponsive: {1}", responsive, unresponsive);
+	}
+
+	public void export() {
+		if(exportFileName.Length > 0) {
+			byte[] bytes = texture.EncodeToPNG();
+
+			File.WriteAllBytes(exportFileName, bytes);
+		}
+	}
+
+
+	private void Update() {
+		if(Input.GetKeyDown(KeyCode.S)) {
+			scan();
+		}
+		else if(Input.GetKeyDown(KeyCode.E)) {
+			evaluate();
+		}
+		else if(Input.GetKeyDown(KeyCode.X)) {
+			export();
+		}
+	}
+}
